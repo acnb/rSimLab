eefb193de1fa2e1597e49683f1dbbc9c62f19dc0
acnb <code@bietenbeck.net>
2016-11-03 15:41:29 +0100

    fix for R CMD check of examples; 'T' and 'F' in Roxygen example environments are confused with 'T'/'F' TRUE/FALSE

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,11 @@
 objFromEnv <- function(env){
   nms <- ls(envir = env, sorted = FALSE)
+  # fix for R CMD check of examples ('T' and 'F' in Roxygen example
+  # environments are confused with 'T'/'F' TRUE/FALSE)
+  nms <- nms[nms != 'T' & nms != 'F']
   obj.ls <- mget(nms, envir = env)
   res <- lapply(obj.ls, function(o)
-    ifelse(is.environment(o) |is.function(o), NA, o))
+    ifelse(is.environment(o) | is.function(o), NA, o))
   res[!is.na(res)]
 }
 
